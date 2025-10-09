4a7ccb2273f24875702f095a45ba135ed51bcb80
Oumar3 <oumaralitori1@gmail.com>
2025-10-09 22:15:56 +0100

    Refactor rate limiter implementation for improved clarity and functionality

--- pokemonserver/server/middleware/rateLimiter.js
+++ pokemonserver/server/middleware/rateLimiter.js
@@ -1,11 +1,14 @@
 const rateLimit = require('express-rate-limit');
 
-const rateLimiter = rateLimit({
-  windowMs: 60 * 60 * 1000, // minutes * secondes * millisecondes
-  max: 100, // 10 requêtes par IP
-  legacyHeaders: false, // Désactiver les en-têtes `X-RateLimit-*`
-  standardHeaders: true, // Activer les en-têtes `RateLimit-*`
-  message: { error: "Trop de requêtes, veuillez réessayer plus tard." },
-});
+const rateLimiter = (req,res,next) => {
+  const limiter = rateLimit({
+    windowMs: 60 * 60 * 1000, // minutes * secondes * millisecondes
+    max: 100, // 100 requêtes par IP
+    legacyHeaders: false, // Désactiver les en-têtes `X-RateLimit-*`
+    standardHeaders: true, // Activer les en-têtes `RateLimit-*`
+    message: { error: "Trop de requêtes, veuillez réessayer plus tard." },
+  });
+  limiter(req, res, next);
+};
 
 module.exports = rateLimiter;
